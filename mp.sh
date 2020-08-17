d8810125b0bd9ae41b47d3c7e46e3d8a7e3c6315
Rafael Ramos <arkanjo.ms@gmail.com>
2020-08-17 09:07:54 -0300

    Updating utility script.

--- mp.sh
+++ mp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 STACK_NAME="mp"
-case "$1" in
+case "${1}" in
 start)
   echo "Starting ${STACK_NAME} stack"
   docker-compose -p ${STACK_NAME} up -d --remove-orphans
@@ -19,7 +19,7 @@ clean)
   ;;
 build)
   echo "Building images"
-  docker-compose build $2
+  docker-compose build "${2}"
   ;;
 *)
   echo "Accepted commands: start, stop, restart, clean and build"
